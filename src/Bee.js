af8b4dca948f9550cd9219e28d09e759ad4411a0
anes <marzoukianas@gmail.com>
2020-03-20 15:51:38 +0100

    Grub, Bee & HoneyMakerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
+// create a Bee class, in pseudoClassical style
+
 var Bee = function() {
+  // calls the Grub S.class on the Bee class
+  Grub.call(this);
+  // Bee's props
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
 };
 
+// setting up the constructo & the prototype for the Bee class
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  // Calls the Bee superclass on this class
+  Bee.call(this);
+  // owns props
+  this.age = 10;
+  this.honeyPot = 0;
+  this.job = "make honey";
 };
 
+// setting the constructor & the prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// Methods for the HoneyMakerBee class
+// makeHoney
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+// giveHoney
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
