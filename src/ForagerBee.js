eebf9e8856ca56b7aba0439d9d37964946a3cf4c
anes <marzoukianas@gmail.com>
2020-03-20 15:59:16 +0100

    forager

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  // calls Bee on this (color, food, eat)
+  Bee.call(this);
+  this.age = 10;
+  this.job = "find pollen";
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// Constructor
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+// Methods
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
